f537c59b0a5c5dc463c896f2816861e5464d5117
helderjossiasmate <movinethjm@gmail.com>
2020-06-21 03:55:39 +0300

    writing the last changes in te app
    In this commit I write the code of the chart, cards and the country picker.
    I also add some css to the recent added components and the image of the app

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { fetchCountries } from '../../api';
+import { NativeSelect, FormControl } from '@material-ui/core';
+import styles from './CountryPicker.module.css';
 
-const CountryPicker = () => {
+const CountryPicker = ({ handleCountryChange }) => {
+    const [fetchedCountries, setFetchedCountries] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setFetchedCountries(await fetchCountries());
+        }
+
+        fetchAPI();
+    }, [setFetchedCountries]);
+
+
     return(
-        <h1>CoutryPicker</h1>
+        <FormControl className={styles.formControl}>
+            <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+                <option value="">Estatistica Global</option>
+    {fetchedCountries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+            </NativeSelect>
+        </FormControl>
     );
 };
 
